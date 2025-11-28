9cfb20de887de7c5755063c7737a1abe22e353fb
Jhon Chris Bautista <jhonchrisgbautista@iskolarngbayan.pup.edu.ph>
2025-11-29 05:44:16 +0800

    Your changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+//asdadwdasd
 class DatabaseSeeder extends Seeder
 {
     /**
